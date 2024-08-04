dc86a419b36b1c8b4a547fce6eafa7c0e77f3e04
dragosbatineanu <48174473+dragosbatineanu@users.noreply.github.com>
2024-08-04 20:33:03 +0300

    - Added a small CLI menu for the first options.

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,36 +10,83 @@ int main() {
 	//declaration of variables
 	int highestNumber;
 	int amountNumbers;
+	int option;
+
 	bool answer = true;
 	bool unique;
 
+	//CLI Menu
+	std::cout << s_nameMessage << std::endl;
+	std::cout << s_breakerHorizontal << std::endl;
+	std::cout << s_singleNumber << std::endl;
+	std::cout << s_MultipleNumber << std::endl;
+	std::cout << s_exit << std::endl;
+	std::cin >> option;
+	std::cout << s_breakerHorizontal << std::endl;
 
-	std::cout << s_rangeNumbers << std::endl;
-	std::cout << s_highestNumbers << std::endl;
-	std::cin >> highestNumber;
-	std::cout << s_amountNumbers << std::endl;
-	std::cin >> amountNumbers;
+	switch (option) 
+	{
+		case 1:
+				std::cout << s_highestNumbers << std::endl;
+				std::cin >> highestNumber;
 
+				while (answer)
+				{
 
-	if (amountNumbers == 1) {
-		while (answer) {
-			if (std::cin && (answer == 0 || answer == 1)) {
-				randomNumber(highestNumber);
-				std::cout << "Another number?" << std::endl;
-				std::cin >> answer;
-			}
-			else {
-				std::cout << "Error wrong answer." << std::endl;
-				break;
-			}
-		}
+					if (std::cin && (answer == 0 || answer == 1))
+					{
+						randomNumber(highestNumber);
+						std::cout << "Another number?" << std::endl;
+						std::cin >> answer;
+					}
+					else
+					{
+						std::cout << "Error wrong answer." << std::endl;
+						break;
+					}
+				}
+			//}
+
+		case 2:
+				std::cout << s_rangeNumbers << std::endl;
+				std::cout << s_highestNumbers << std::endl;
+				std::cin >> highestNumber;
+				std::cout << s_amountNumbers << std::endl;
+				std::cin >> amountNumbers;
+				std::cout << "Do they need to be unique numbers?" << std::endl;
+				std::cin >> unique;
+				uniqueNumbers(highestNumber, amountNumbers, unique);
+		case 3:
+			return 0;
 	}
 
-	if (amountNumbers > 1) {
-		std::cout << "Do they need to be unique numbers?" << std::endl;
-		std::cin >> unique;
-		uniqueNumbers(highestNumber, amountNumbers, unique);
-	}
+	//Strings.h contains those strings
+	//std::cout << s_rangeNumbers << std::endl;
+	//std::cout << s_highestNumbers << std::endl;
+	//std::cin >> highestNumber;
+	//std::cout << s_amountNumbers << std::endl;
+	//std::cin >> amountNumbers;
+
+
+	//if (amountNumbers == 1) {
+	//	while (answer) {
+	//		if (std::cin && (answer == 0 || answer == 1)) {
+	//			randomNumber(highestNumber);
+	//			std::cout << "Another number?" << std::endl;
+	//			std::cin >> answer;
+	//		}
+	//		else {
+	//			std::cout << "Error wrong answer." << std::endl;
+	//			break;
+	//		}
+	//	}
+	//}
+
+	//if (amountNumbers > 1) {
+	//	std::cout << "Do they need to be unique numbers?" << std::endl;
+	//	std::cin >> unique;
+	//	uniqueNumbers(highestNumber, amountNumbers, unique);
+	//}
 
 
 
--- src/Strings.h
+++ src/Strings.h
@@ -2,6 +2,16 @@
 
 #include <string>
 
+//Menu strings
+std::string s_nameMessage = "Welcome to Random Number Generator";
+std::string s_breakerHorizontal = "---------------------------------";
+
+//Menu choices
+std::string s_singleNumber = "1.Random Single Number";
+std::string s_MultipleNumber = "2.Multiple Random Numbers";
+
+std::string s_exit = "3.Exit";
+
 //Strings for easy of change and maintanability
 std::string s_rangeNumbers		= "Enter the range of numbers:";
 std::string s_highestNumbers	= "Highest number:";
