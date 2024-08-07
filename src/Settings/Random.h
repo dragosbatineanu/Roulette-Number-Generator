dc9364139301a8350faeec16a6624efbb778610c
dragosbatineanu <48174473+dragosbatineanu@users.noreply.github.com>
2024-08-07 22:14:27 +0300

    - Expanded main Random class - Added virtual functions for future classes

--- src/Settings/Random.h
+++ src/Settings/Random.h
@@ -1,6 +1,18 @@
 #pragma once
 class Random
 {
+private:
+	int highestNumber;
+	int amountNumbers;
+	bool unique;
+
+public:
+
+	Random(int h, int a, bool u);
+
+	virtual int setHighestNumber(int h) const = 0;
+	virtual int setAmountNumbers(int a) const = 0;
+	virtual bool setUnique(bool u) const = 0;
 
 };
 
